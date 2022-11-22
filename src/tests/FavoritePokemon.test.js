05acc61436a2ab8eb6ef25e4f45e46c98ff52496
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-11-22 16:04:31 -0300

    adiciona teste para <FavoritePokemon />

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('<FavoritePokemon />', () => {
+  it('testa  Página Favorites ', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const heading = screen.getByRole('heading', { name: 'Favorite Pokémon', level: 2 });
+    const paragrafo = screen.getByText('No favorite Pokémon found');
+
+    expect(heading).toBeInTheDocument();
+    expect(paragrafo).toBeInTheDocument();
+  });
+
+  it('mesma snapshot', () => {
+    const { container } = renderWithRouter(<FavoritePokemon />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
