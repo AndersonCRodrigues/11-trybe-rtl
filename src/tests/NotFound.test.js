2c6673b305d8c9bf4ff4664e6fd835c2b5699d0a
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-11-22 16:10:34 -0300

    adiciona teste para <NotFound />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('', () => {
+  it('testa se um caminho errado leva para "Not Found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', { name: 'Page requested not found', level: 2 });
+    const image = screen.getByRole('img', { name: 'Pikachu crying because the page requested was not found' });
+    const imagemSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(heading).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', imagemSrc);
+  });
+
+  it('mesma snapshot', () => {
+    const { container } = renderWithRouter(<NotFound />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
